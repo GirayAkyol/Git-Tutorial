2ef96c1a38290cadf9ff164dc93fcb7b1c8d68f2
gitu <gitu@lgq>
2025-02-12 15:10:17 +0300

    Division

--- test.sh
+++ test.sh
@@ -3,6 +3,9 @@
 # Define test cases as an array of tuples (input, expected_output)
 test_cases=(
     "2+2,4.0"
+    "5-2,3.0"
+    "2*3,6.0"
+    "6/2,3.0"
 )
 
 # Function to run a single test case
